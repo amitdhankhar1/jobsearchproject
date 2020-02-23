3cf8e6a15ac38731ff1b47cd49e68eba7e42f758
amit dhankhar <amitdhankhar616@gmail.com>
2020-02-23 23:38:57 +0530

    add file

---
+++ middle.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server=http.createServer((req,res)=>{
+    console.log('request for'+req.url+'by method'+req.method)
+
+    res.statusCode=200;
+    res.setHeader('content-type','text/html');
+    res.end('<html><body><h1> conection success....</h1></body> </html>');
+
+});
+server.listen(3000,function(){});
