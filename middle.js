d8501c20debcf0bcee5309260c79a34bd4fc7820
amit dhankhar <amitdhankhar616@gmail.com>
2020-02-23 23:41:07 +0530

    add file

--- middle.js
+++ middle.js
@@ -1,11 +1,16 @@
-const http = require('http');
+    const http = require('http');
 
-const server=http.createServer((req,res)=>{
+    const server=http.createServer((req,res)=>{
+
     console.log('request for'+req.url+'by method'+req.method)
 
     res.statusCode=200;
+
     res.setHeader('content-type','text/html');
+
     res.end('<html><body><h1> conection success....</h1></body> </html>');
 
-});
-server.listen(3000,function(){});
+    });
+
+    
+    server.listen(3000,function(){});
